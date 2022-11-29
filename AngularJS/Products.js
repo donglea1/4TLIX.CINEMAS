e5012f255af6b0acaa418b43f7a7b6bd0ea7b115
Khanh <hoangkhanh2420@gmail.com>
2022-11-30 03:59:41 +0700

    up

--- AngularJS/Products.js
+++ AngularJS/Products.js
@@ -11,15 +11,15 @@ app.controller("shopping_cart", function ($scope, $window) {
     ];
 
     $scope.seats = [
-        { seat_id: "a1", seat_id: "a2", seat_id: "a3", seat_id: "a4", seat_id: "a5", seat_id: "a6", seat_id: "a7", seat_id: "a8", seat_id: "a9", seat_id0: "a10", seat_id1: "a11" },
-        { seat_id: "b1", seat_id: "b2", seat_id: "b3", seat_id: "b4", seat_id: "b5", seat_id: "b6", seat_id: "b7", seat_id: "b8", seat_id: "b9", seat_id0: "b10", seat_id1: "b11" },
-        { seat_id: "c1", seat_id: "c2", seat_id: "c3", seat_id: "c4", seat_id: "c5", seat_id: "c6", seat_id: "c7", seat_id: "c8", seat_id: "c9", seat_id0: "c10", seat_id1: "c11" },
-        { seat_id: "d1", seat_id: "d2", seat_id: "d3", seat_id: "d4", seat_id: "d5", seat_id: "d6", seat_id: "d7", seat_id: "d8", seat_id: "d9", seat_id0: "d10", seat_id1: "d11" },
-        { seat_id: "e1", seat_id: "e2", seat_id: "e3", seat_id: "e4", seat_id: "e5", seat_id: "e6", seat_id: "e7", seat_id: "e8", seat_id: "e9", seat_id0: "e10", seat_id1: "e11" },
-        { seat_id: "f1", seat_id: "f2", seat_id: "f3", seat_id: "f4", seat_id: "f5", seat_id: "f6", seat_id: "f7", seat_id: "f8", seat_id: "f9", seat_id0: "f10", seat_id1: "f11" },
-        { seat_id: "g1", seat_id: "g2", seat_id: "g3", seat_id: "g4", seat_id: "g5", seat_id: "g6", seat_id: "g7", seat_id: "g8", seat_id: "g9", seat_id0: "g10", seat_id1: "g11" },
-        { seat_id: "h1", seat_id: "h2", seat_id: "h3", seat_id: "h4", seat_id: "h5", seat_id: "h6", seat_id: "h7", seat_id: "h8", seat_id: "h9", seat_id0: "h10", seat_id1: "h11" },
-        { seat_id: "i1", seat_id: "i2", seat_id: "i3", seat_id: "i4", seat_id: "i5", seat_id: "i6", seat_id: "i7", seat_id: "i8", seat_id: "i9", seat_id0: "i10", seat_id1: "i11" },
+        { seat_id1: "a1", seat_id2: "a2", seat_id3: "a3", seat_id4: "a4", seat_id5: "a5", seat_id6: "a6", seat_id7: "a7", seat_id8: "a8", seat_id9: "a9", seat_id10: "a10" },
+        { seat_id1: "b1", seat_id2: "b2", seat_id3: "b3", seat_id4: "b4", seat_id5: "b5", seat_id6: "b6", seat_id7: "b7", seat_id8: "b8", seat_id9: "b9", seat_id10: "b10" },
+        { seat_id1: "c1", seat_id2: "c2", seat_id3: "c3", seat_id4: "c4", seat_id5: "c5", seat_id6: "c6", seat_id7: "c7", seat_id8: "c8", seat_id9: "c9", seat_id10: "c10" },
+        { seat_id1: "d1", seat_id2: "d2", seat_id3: "d3", seat_id4: "d4", seat_id5: "d5", seat_id6: "d6", seat_id7: "d7", seat_id8: "d8", seat_id9: "d9", seat_id10: "d10" },
+        { seat_id1: "e1", seat_id2: "e2", seat_id3: "e3", seat_id4: "e4", seat_id5: "e5", seat_id6: "e6", seat_id7: "e7", seat_id8: "e8", seat_id9: "e9", seat_id10: "e10" },
+        { seat_id1: "f1", seat_id2: "f2", seat_id3: "f3", seat_id4: "f4", seat_id5: "f5", seat_id6: "f6", seat_id7: "f7", seat_id8: "f8", seat_id9: "f9", seat_id10: "f10" },
+        { seat_id1: "g1", seat_id2: "g2", seat_id3: "g3", seat_id4: "g4", seat_id5: "g5", seat_id6: "g6", seat_id7: "g7", seat_id8: "g8", seat_id9: "g9", seat_id10: "g10" },
+        { seat_id1: "h1", seat_id2: "h2", seat_id3: "h3", seat_id4: "h4", seat_id5: "h5", seat_id6: "h6", seat_id7: "h7", seat_id8: "h8", seat_id9: "h9", seat_id10: "h10" },
+        { seat_id1: "i1", seat_id2: "i2", seat_id3: "i3", seat_id4: "i4", seat_id5: "i5", seat_id6: "i6", seat_id7: "i7", seat_id8: "i8", seat_id9: "i9", seat_id10: "i10" },
 
     ];
 
@@ -55,13 +55,17 @@ app.controller("shopping_cart", function ($scope, $window) {
         $scope.updatePrice();
     };
 
+    $scope.limitSeat = 0;
 
     $scope.updatePrice = function () {
         var totalPrice = 0;
+        var totalQty = 0;
         for (var i = 0; i < $scope.carts.length; i++) {
             totalPrice += ($scope.carts[i].qty) * ($scope.carts[i].price);
+            totalQty += ($scope.carts[i].qty);
         }
         $scope.totalPrice = totalPrice;
+        return $scope.limitSeat = totalQty;
     };
 
 
@@ -74,4 +78,19 @@ app.controller("shopping_cart", function ($scope, $window) {
         $scope.updatePrice();
     }
 
+    $scope.selected = [];
+
+    $scope.toggle = function (item, list) {
+        var idx = list.indexOf(item);
+        if (idx > -1) {
+            list.splice(idx, 1);
+        }else {
+            list.push(item);
+        }
+
+    };
+
+    $scope.exists = function (item, list) {
+        return list.indexOf(item) > -1;
+    };
 });
